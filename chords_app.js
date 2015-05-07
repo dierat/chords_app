24630eb422b6a74c430f17cf06690f43ae1c7c64
dierat <diedra.rater@gmail.com>
2015-05-06 17:47:28 -0700

    adds fields to Chords documents

--- chords_app.js
+++ chords_app.js
@@ -14,8 +14,15 @@ if (Meteor.isServer) {
       var chords_array = [['C', 'G'], ['B#', 'G'], ['Am', 'E']];
       _.each(chords_array, function(chord) {
         Chords.insert({
+          user_id: null,
           chord1: chord[0],
-          chord2: chord[1]
+          chord2: chord[1],
+          created: new Date(),
+          last_practice: null,
+          total_practice: 0,
+          best_num: null,
+          average_num: null,
+          practice: {}
         });
       });
     }
